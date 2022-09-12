b76c3638278021ec50b8f988e76e2257cad8cd0e
Marcus Silva <marcusluis.mail@gmail.com>
2022-09-12 00:05:25 -0300

    highlight visited nav links only with css

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const currentPage = window.location.pathname;
-const navLinks = document.querySelectorAll("nav a");
-
-navLinks.forEach((link) => {
-    if (link.href.includes(`${currentPage}`)) {
-        link.classList.add("active");
-    }
-});
