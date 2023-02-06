31258e0561425c0de02f1bf6f0e37cfb73d082a2
Marcus Silva <marcusluis.mail@gmail.com>
2023-02-06 09:53:07 -0300

    Contact component criado

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,18 @@
+import { GithubLogo } from "phosphor-react"
+import styles from "./Contact.module.css"
+
+function Contact() {
+    return (
+        <div className={styles.ContactContainer}>
+            <a
+                href="https://github.com/m-rcussilva"
+                target="_blank"
+                className={styles.socialMediaIcons}
+            >
+                <GithubLogo size={32} color="#c1fd01" />
+            </a>
+        </div>
+    )
+}
+
+export default Contact
