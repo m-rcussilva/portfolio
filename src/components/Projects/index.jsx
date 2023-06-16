426fce60d64bf6555a48825fa608e2f9427ae394
Marcus Silva <marcusluis.mail@gmail.com>
2023-06-16 14:09:33 -0300

    rebuilding

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -10,56 +10,13 @@ function Projects() {
                 <section>
                     <article>
                         <a
-                            href="https://github.com/m-rcussilva/ruby/tree/main/markdownProcessor"
+                            href="#"
                             target="_blank"
                         >
-                            Markdown Processor é um CLI tool que converte
-                            arquivo .md para .html
+                            Test
                         </a>
                         <hr />
-                    </article>
-
-                    <article>
-                        <a
-                            href="https://github.com/m-rcussilva/ruby/tree/main/spellchecker"
-                            target="_blank"
-                        >
-                            SpellChecker - CLI que verifica o nome das linguages
-                            de programção
-                        </a>
-                        <hr />
-                    </article>
-
-                    <article>
-                        <a
-                            href="https://unsplashapi-app-searchimages.surge.sh/"
-                            target="_blank"
-                        >
-                            App que busca por fotos utilizando unsplash api
-                        </a>
-                        <hr />
-                    </article>
-
-                    <article>
-                        <a
-                            href="https://avatar-editor-vuejs.surge.sh/"
-                            target="_blank"
-                        >
-                            Edite um avatar - Aprendendo fundamentos do Vue JS 2
-                        </a>
-                        <hr />
-                    </article>
-
-                    <article>
-                        <a
-                            href="https://simpecrudapp.surge.sh/"
-                            target="_blank"
-                        >
-                            Um App simples feito com JS puro que permite
-                            escrever, editar e excluir mensagens
-                        </a>
-                        <hr />
-                    </article>
+                    </article> 
                 </section>
             </div>
         </div>
